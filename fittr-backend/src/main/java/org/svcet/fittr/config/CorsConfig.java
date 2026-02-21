fa0ff197f8f37f9d23ccc81ef4ebaa3e325769ad
TanojGowrabathina <tanugowrabathina@gmail.com>
2026-02-22 00:31:56 +0530

    Fix ESLint errors for Vercel

--- fittr-backend/src/main/java/org/svcet/fittr/config/CorsConfig.java
+++ fittr-backend/src/main/java/org/svcet/fittr/config/CorsConfig.java
@@ -1,5 +1,27 @@
-package org.svcet.fittr.config;
+// package org.svcet.fittr.config;
 
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// @Configuration
+// public class CorsConfig {
+
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//             @Override
+//             public void addCorsMappings(CorsRegistry registry) {
+//                 registry.addMapping("/**")
+//                     .allowedOrigins("http://localhost:3000")
+//                     .allowedMethods("*")
+//                     .allowedHeaders("*")
+//                     .allowCredentials(false);
+//             }
+//         };
+//     }
+// }
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,10 +36,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
-                    .allowedMethods("*")
-                    .allowedHeaders("*")
-                    .allowCredentials(false);
+                        .allowedOrigins("https://fitness-app-two-vivid.vercel.app")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
             }
         };
     }
