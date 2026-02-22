5d8002f84f09d3ed49971b9435f67f6530964528
TanojGowrabathina <tanugowrabathina@gmail.com>
2026-02-22 08:22:56 +0530

    Fix spring boot cors

--- fittr-backend/src/main/java/org/svcet/fittr/config/CorsConfig.java
+++ fittr-backend/src/main/java/org/svcet/fittr/config/CorsConfig.java
@@ -1,27 +1,5 @@
 package org.svcet.fittr.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-// @Configuration
-// public class CorsConfig {
-
-//     @Bean
-//     public WebMvcConfigurer corsConfigurer() {
-//         return new WebMvcConfigurer() {
-//             @Override
-//             public void addCorsMappings(CorsRegistry registry) {
-//                 registry.addMapping("/**")
-//                     .allowedOrigins("http://localhost:3000")
-//                     .allowedMethods("*")
-//                     .allowedHeaders("*")
-//                     .allowCredentials(false);
-//             }
-//         };
-//     }
-// }
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -36,11 +14,33 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                    .allowedOrigins("http://localhost:3000","https://fitness-app-two-virid.vercel.app/")
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    .allowCredentials(false);
             }
         };
     }
 }
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// @Configuration
+// public class CorsConfig {
+
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//             @Override
+//             public void addCorsMappings(CorsRegistry registry) {
+//                 registry.addMapping("/**")
+//                         .allowedOriginPatterns("*")
+//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                         .allowedHeaders("*")
+//                         .allowCredentials(true);
+//             }
+//         };
+//     }
+// }
